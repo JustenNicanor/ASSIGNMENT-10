f45bb4534baf6cf6ba49fa10666ec7f7c320cbf1
JustenNicanor <92636904+JustenNicanor@users.noreply.github.com>
2022-02-15 22:20:45 +0800

    Third commit(Adding codes to store the QR datas in text file)

--- Assignment 10(1).py
+++ Assignment 10(1).py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 from pyzbar.pyzbar import decode
-
+global Info
 
 Webcam = cv2.VideoCapture(0)
 
@@ -10,11 +10,13 @@ while True:
     success, image = Webcam.read()
     for barcode in decode(image):
         Info = barcode.data.decode('utf-8')
+        with open('QRdata.txt', 'w') as Textfile:
+            Textfile.write(Info)
+
         print(Info)
-
-
+        
     cv2.imshow('SCANNER', image)
     cv2.waitKey(0)
 
-
+    
 
