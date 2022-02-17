472d2aa10c82900419d87f83899739f682d12680
JustenNicanor <92636904+JustenNicanor@users.noreply.github.com>
2022-02-18 00:24:28 +0800

    Fourth Commit(Final! Coding the date and time and filming for demo)

--- Assignment 10(1).py
+++ Assignment 10(1).py
@@ -1,21 +1,27 @@
 import cv2
-import numpy as np
+from cv2 import VideoCapture
 from pyzbar.pyzbar import decode
-global Info
-
+import datetime as dt
 Webcam = cv2.VideoCapture(0)
 
 while True:
 
-    success, image = Webcam.read()
-    for barcode in decode(image):
+    success, camera = Webcam.read()
+    for barcode in decode(camera):
         Info = barcode.data.decode('utf-8')
+        timedate = dt.datetime.now()              
+        Time = timedate.strftime("%H:%M")
+        Date = timedate.strftime("%B %d, %Y")
         with open('QRdata.txt', 'w') as Textfile:
-            Textfile.write(Info)
+            Textfile.write (Info + (f"\n\n\nDate: {Date}\nTime: {Time}"))             
+            Textfile.close()
 
         print(Info)
+        print(f"\n\n\nDate: {Date}\nTime: {Time}")
+    
         
-    cv2.imshow('SCANNER', image)
+        
+    cv2.imshow('SCANNER',camera)
     cv2.waitKey(0)
 
     
